53defef707f57137376482e97b9d659dbdb1fb35
idony <aptemhh@mail.ru>
2018-02-09 22:19:05 +0600

    - подключил к mysql

--- src/main/java/ru/rest/youth/songs/RestService.java
+++ src/main/java/ru/rest/youth/songs/RestService.java
@@ -26,7 +26,7 @@ public class RestService  {
     private Boolean uploadOnline;
 
     @GET
-    @Path("/number/{id}")
+    @Path("/number/{id}"+ ";charset=utf-8")
     @Produces(MediaType.APPLICATION_JSON)
     @Cacheable(value = "employee", key = "#id")
     public String getSong(@PathParam("id") Integer id) {
@@ -35,7 +35,7 @@ public class RestService  {
 
     @GET
     @Path("/find/{text}")
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON+ ";charset=utf-8")
     @Cacheable(value = "employee", key = "#text")
     public String findSongs(@PathParam("text") String text) {
         return JsonConverter.convert(jdbcSongDAO.getSongListShort(text));
@@ -43,7 +43,7 @@ public class RestService  {
 
     @GET
     @Path("/find/")
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON+ ";charset=utf-8")
     @Cacheable(value = "employee")
     public String findSongs() {
         return JsonConverter.convert(jdbcSongDAO.getSongListShort(null));
@@ -65,8 +65,8 @@ public class RestService  {
 
     @GET
     @Path("/upload")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON+ ";charset=utf-8")
+    @Consumes(MediaType.APPLICATION_JSON+ ";charset=utf-8")
     @CacheEvict(value = "employee")
     public Response upload(Song song) {
         if (uploadOnline) {
@@ -77,7 +77,7 @@ public class RestService  {
     }
 
     @GET
-    @Path("/download")
+    @Path("/download"+ ";charset=utf-8")
     @Cacheable(value = "employee")
     @Produces(MediaType.TEXT_HTML)
     public Response downloadXml(@Context HttpServletResponse response) throws IOException {
